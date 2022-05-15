4f48658ec1c6d42989a1ab090f5d58dbd08c5451
kevinsmasters <gmail@kevinmasters.com>
2022-05-14 21:54:51 -0400

    removes api key

--- src/api/index.js
+++ src/api/index.js
@@ -27,8 +27,8 @@ export const getWeatherData = async (lat, lng) => {
     const { data } = await axios.get('https://community-open-weather-map.p.rapidapi.com/find', {
       params: { lon: lng, lat: lat },
       headers: {
-        'X-RapidAPI-Host': 'community-open-weather-map.p.rapidapi.com',
-        'X-RapidAPI-Key': 'ec6d2bb011mshfb28ffd4979b16bp1ced94jsne9cfec7e9f7a'
+        'X-RapidAPI-Host': process.env.REACT_APP_RAPID_API_WHOST,
+        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_WKEY
       }
     });
     return data;
